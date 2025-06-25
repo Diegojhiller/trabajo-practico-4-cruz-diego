e962552aa032c5bac8ce6afe6bac82fd769dd860
diegojcruz <diegohiller754@gmail.com>
2025-06-24 22:33:50 -0300

    Inicio del trabajo praactico 4 con la estructura pedida en el enunciado(carpetas y archivos)

---
+++ app.js
@@ -0,0 +1,22 @@
+// src/app.js
+import express from "express";
+import dotenv from "dotenv";
+
+// Cargar variables de entorno desde .env
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000; // Obtiene el puerto de las variables de entorno o usa 3000 por defecto
+
+// Middleware para parsear JSON en las solicitudes
+app.use(express.json());
+
+// Ruta de prueba (opcional, para verificar que el servidor funciona)
+app.get("/", (req, res) => {
+  res.send("¡API de Dragon Ball funcionando!");
+});
+
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
+});
