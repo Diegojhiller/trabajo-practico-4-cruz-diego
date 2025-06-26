00cb27a18fd7da6204b0853ae57a3710b4b91777
Diego Cruz <diegohiller754@gmail.com>
2025-06-26 10:08:55 -0300

    creación de servidor básico de express que lee las variables de entorno

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
-// src/app.js
 import express from "express";
 import dotenv from "dotenv";
 
-// Cargar variables de entorno desde .env
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000; // Obtiene el puerto de las variables de entorno o usa 3000 por defecto
-
-// Middleware para parsear JSON en las solicitudes
 app.use(express.json());
 
-// Ruta de prueba (opcional, para verificar que el servidor funciona)
-app.get("/", (req, res) => {
-  res.send("¡API de Dragon Ball funcionando!");
-});
+const PORT = process.env.PORT || 4000; 
+
+
+
 
 // Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor escuchando en http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
 });
